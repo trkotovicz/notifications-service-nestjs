34c6292c7c74f801aba41977eeba609a12b05e8c
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-12-17 17:50:24 -0300

    feat: created notification repository

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,15 +1,26 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationRepositoryMock = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepositoryMock);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'uma notificação legal',
       category: 'social',
       recipientId: 'example-recipient-id',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
