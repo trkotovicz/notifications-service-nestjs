f1bcd842d7603d63a6ed63a3f32fa3312476b53d
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-05-16 16:02:41 -0300

    feat: created the canceled notification method

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -8,9 +8,17 @@ import { PrismaService } from '../prisma.service';
 export class PrismaNotificationsRepository implements NotificationRepository {
   constructor(private prismaService: PrismaService) {}
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
 
     await this.prismaService.notification.create({ data: raw });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
